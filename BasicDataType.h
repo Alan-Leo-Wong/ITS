33316e74a3a5d211c1a8076a090d40da40ba5cf4
Lei Wang <602289550@qq.com>
2023-03-10 21:54:12 +0800

    fix octree bug and complete intersection between mesh's edge and box

--- BasicDataType.h
+++ BasicDataType.h
@@ -29,6 +29,8 @@ using PII = std::pair<int, int>;
 using PDD = std::pair<double, double>;
 using PV3d = std::pair<V3d, V3d>;
 
+constexpr double DINF = (std::numeric_limits<double>::max)();
+
 //struct double3
 //{
 //	double x, y, z;
@@ -39,3 +41,76 @@ using PV3d = std::pair<V3d, V3d>;
 //
 //	double3& operator=(const double3& other) { x = other.x, y = other.y, z = other.z; }
 //};
+
+struct Triangle
+{
+	V3d p1, p2, p3;
+
+	Triangle() = default;
+
+	Triangle(const V3d& _p1, const V3d& _p2, const V3d& _p3) :p1(_p1), p2(_p2), p3(_p3) {}
+
+	V3d cpCoefficientOfTriangle(const double& x, const double& y, int flag)
+	{
+		V3d d1 = p2 - p1;
+		V3d d2 = p3 - p1;
+		if (flag == 0)
+		{
+			V3d intersection = V3d(x, y, 0.0);
+			double area = d1.cross(d2).z();
+			if (abs(area) < 1e-10) return V3d(-1.0, -1.0, -1.0);
+			V3d v1 = p1 - intersection;
+			V3d v2 = p2 - intersection;
+			V3d v3 = p3 - intersection;
+
+			double area1 = v2.cross(v3).z();
+			double lambda1 = area1 / area;
+			if (lambda1 < 0) return V3d(-1.0, -1.0, -1.0);
+			area1 = v3.cross(v1).z();
+			double lambda2 = area1 / area;
+			if (lambda2 < 0) return V3d(-1.0, -1.0, -1.0);
+			area1 = v1.cross(v2).z();
+			double lambda3 = area1 / area;
+			if (lambda3 < 0) return V3d(-1.0, -1.0, -1.0);
+			return V3d(lambda1, lambda2, lambda3);
+		}
+		if (flag == 1)
+		{
+			V3d intersection(0.0, x, y);
+			double area = d1.cross(d2).x();
+			if (abs(area) < 1e-10) return V3d(-1.0, -1.0, -1.0);
+			V3d v1 = p1 - intersection;
+			V3d v2 = p2 - intersection;
+			V3d v3 = p3 - intersection;
+
+			double area1 = v2.cross(v3).x();
+			double lambda1 = area1 / area;
+			if (lambda1 < 0) return V3d(-1.0, -1.0, -1.0);
+			area1 = v3.cross(v1).x();
+			double lambda2 = area1 / area;
+			if (lambda2 < 0) return V3d(-1.0, -1.0, -1.0);
+			area1 = v1.cross(v2).x();
+			double lambda3 = area1 / area;
+			if (lambda3 < 0) return V3d(-1.0, -1.0, -1.0);
+			return V3d(lambda1, lambda2, lambda3);
+		}
+
+		V3d intersection = V3d(y, 0.0, x);
+		double area = d1.cross(d2).y();
+		if (abs(area) < 1e-10) return V3d(-1.0, -1.0, -1.0);
+		V3d v1 = p1 - intersection;
+		V3d v2 = p2 - intersection;
+		V3d v3 = p3 - intersection;
+
+		double area1 = v2.cross(v3).y();
+		double lambda1 = area1 / area;
+		if (lambda1 < 0) return V3d(-1.0, -1.0, -1.0);
+		area1 = v3.cross(v1).y();
+		double lambda2 = area1 / area;
+		if (lambda2 < 0) return V3d(-1.0, -1.0, -1.0);
+		area1 = v1.cross(v2).y();
+		double lambda3 = area1 / area;
+		if (lambda3 < 0) return V3d(-1.0, -1.0, -1.0);
+		return V3d(lambda1, lambda2, lambda3);
+	}
+};
